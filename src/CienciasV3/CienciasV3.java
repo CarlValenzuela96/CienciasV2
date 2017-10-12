8fff15e35e836687d8311679f549cc6390008d34
Carlos <Carlos@DESKTOP-2NCUI3D>
2017-10-12 12:21:05 -0300

    -Se elimino POO y se hizo estructurado -Se implementó programacion dinamica para el calculo de cantidad de permutaciones

--- src/CienciasV3/CienciasV3.java
+++ src/CienciasV3/CienciasV3.java
@@ -5,6 +5,8 @@
  */
 package CienciasV3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -16,28 +18,75 @@ public class CienciasV3 {
     /**
      * @param args the command line arguments
      */
+    private static ArrayList list = new ArrayList<>();
+    static HashMap<Integer, Long> memo = new HashMap<>();
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner leer = new Scanner(System.in);
-       
-        
+
+        Scanner read = new Scanner(System.in);
+        //Se ingresa el numero a permutar
         System.out.println("ingrese numero");
-        String numero= leer.next();
-        System.out.println("________________");
-        ListaPermutaciones a = new ListaPermutaciones(numero);
-        a.cantidadPermutaciones();
-        System.out.println("________________");
-        long startTime = System.nanoTime();
-        
-        a.permutation("", numero); 
+        String numero = read.next();
        
-        System.out.println("________");
-        System.out.println("Soluciones: "+a.getList().size());
+        long startTime = System.nanoTime();
+        System.out.println("_________");
         
+        //Calcula Cantidad de permutaciones
+        long cantPer= cantidadPermutaciones(numero.length());
+        System.out.println("Cantidad de Permutaciones: "+cantPer);
+        
+        //Calcula Permutaciones y valida permutaciones aceptadas
+        //las permutaciones aceptadas las agrega al ArrayList lista
+        permutation("", numero);
+
+        System.out.println("________");
+        
+        //Imprime cantidad de soluciones
+        System.out.println("Soluciones: " + list.size());
+
         long endTime = System.nanoTime() - startTime;
         System.out.println("_________");
-        System.out.println("Tiempo de Ejecucion: "+endTime/1e6 + " ms");
+        System.out.println("Tiempo de Ejecucion: " + endTime / 1e6 + " ms");
     }
+
+     public static long cantidadPermutaciones(int n) {
+
+        if (n < 2) {
+            return 1;
+        }
+        
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        } else {
+            
+            long a = n * cantidadPermutaciones(n - 1);
+            memo.put(n, a);
+            return a;
+        }
+    }
+     
+    public static void permutation(String prefix, String str) {
+        int n = str.length();
+
+        if (n == 0) {
+           
+            if (Long.parseLong(prefix) % 11 == 0) {
+                if (prefix.charAt(0) != '0') {
+                    if (!list.contains(String.valueOf(prefix))) {
+                        list.add(prefix);
+                    }
+                }
+            }
+
+        } else {
+            for (int i = 0; i < n; i++) {
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+            }
+
+        }
+
+    }
+    
     
 
 }
